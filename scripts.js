e93c16db96b0d2f0b68173b21640c3983b6b8f9f
aliaformo <aliaformo@gmail.com>
2022-07-25 03:26:32 +0200

    allow drag & drop

--- scripts.js
+++ scripts.js
@@ -3,30 +3,75 @@
 
 // +++++++++++++++++++++++++++++++++++++++ Files uploading +++++++++++++++++++++++++++++++++++++++++++++++++
 
-function handleFileSelect(evt) {
-    var files = evt.target.files;
-
-
-    for (var i = 0, f; f = files[i]; i++) {
-
-        if (!f.type.match('.csv' || '.xlsx' || '.xls')) {
-            continue;
-        }
-
-
-        var reader = new FileReader();
-        reader.onload = (function (theFile) {
-            return function (e) {
-                var span = document.createElement('span');
-                span.innerHTML = ['<csv class="thumb" src="', e.target.result,
-                    '" title="', escape(theFile.name), '"/>'
-                ].join('');
-
-                document.getElementById('list').insertBefore(span, null);
-            };
-        })(f);
-
-        reader.readAsDataURL(f);
+document.querySelectorAll(".drop-zone__input").forEach((inputElement) => {
+    const dropZoneElement = inputElement.closest(".drop-zone");
+  
+    dropZoneElement.addEventListener("click", (e) => {
+      inputElement.click();
+    });
+  
+    inputElement.addEventListener("change", (e) => {
+      if (inputElement.files.length) {
+        updateThumbnail(dropZoneElement, inputElement.files[0]);
+      }
+    });
+  
+    dropZoneElement.addEventListener("dragover", (e) => {
+      e.preventDefault();
+      dropZoneElement.classList.add("drop-zone--over");
+    });
+  
+    ["dragleave", "dragend"].forEach((type) => {
+      dropZoneElement.addEventListener(type, (e) => {
+        dropZoneElement.classList.remove("drop-zone--over");
+      });
+    });
+  
+    dropZoneElement.addEventListener("drop", (e) => {
+      e.preventDefault();
+  
+      if (e.dataTransfer.files.length) {
+        inputElement.files = e.dataTransfer.files;
+        updateThumbnail(dropZoneElement, e.dataTransfer.files[0]);
+      }
+  
+      dropZoneElement.classList.remove("drop-zone--over");
+    });
+  });
+  
+  /**
+   * Updates the thumbnail on a drop zone element.
+   *
+   * @param {HTMLElement} dropZoneElement
+   * @param {File} file
+   */
+  function updateThumbnail(dropZoneElement, file) {
+    let thumbnailElement = dropZoneElement.querySelector(".drop-zone__thumb");
+  
+    // First time - remove the prompt
+    if (dropZoneElement.querySelector(".drop-zone__prompt")) {
+      dropZoneElement.querySelector(".drop-zone__prompt").remove();
     }
-}
-document.getElementById('files').addEventListener('change', handleFileSelect, false);
+  
+    // First time - there is no thumbnail element, so lets create it
+    if (!thumbnailElement) {
+      thumbnailElement = document.createElement("div");
+      thumbnailElement.classList.add("drop-zone__thumb");
+      dropZoneElement.appendChild(thumbnailElement);
+    }
+  
+    thumbnailElement.dataset.label = file.name;
+  
+    // Show thumbnail for image files
+    if (file.type.startsWith("image/")) {
+      const reader = new FileReader();
+  
+      reader.readAsDataURL(file);
+      reader.onload = () => {
+        thumbnailElement.style.backgroundImage = `url('${reader.result}')`;
+      };
+    } else {
+      thumbnailElement.style.backgroundImage = null;
+    }
+  }
+  
